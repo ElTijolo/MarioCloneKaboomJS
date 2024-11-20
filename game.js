31fb2cae29d29f5cb2fdac4fc73f39f3e815ed7f
ELTijolo <side.wez@gmail.com>
2024-11-20 14:37:04 +0100

    rewind

--- game.js
+++ game.js
@@ -1,191 +1,228 @@
-kaboom();
-
-scene("main_menu", () => {
-    add([text("Press Enter to Start"), pos(100, 100)]);
-    keyPress("enter", () => {
-        go("game");
-    });
-});
-
-
-scene("game", () => {
+//IMPORTANT: Make sure to use Kaboom version 0.5.0 for this game by adding the correct script tag in the HTML file.
+ 
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 2,
+    debug: true,
+    clearColor: [0, 0, 0, 1],
+  });
+   
+  // Speed identifiers
+  const MOVE_SPEED = 120;
+  const JUMP_FORCE = 360;
+  const BIG_JUMP_FORCE = 550;
+  let CURRENT_JUMP_FORCE = JUMP_FORCE;
+  const FALL_DEATH = 400;
+  const ENEMY_SPEED = 20;
+   
+  // Game logic
+   
+  let isJumping = true;
+   
+  loadRoot("https://i.imgur.com/");
+  loadSprite("coin", "wbKxhcd.png");
+  loadSprite("evil-shroom", "KPO3fR9.png");
+  loadSprite("brick", "pogC9x5.png");
+  loadSprite("block", "M6rwarW.png");
+  loadSprite("mario", "Wb1qfhK.png");
+  loadSprite("mushroom", "0wMd92p.png");
+  loadSprite("surprise", "gesQ1KP.png");
+  loadSprite("unboxed", "bdrLpi6.png");
+  loadSprite("pipe-top-left", "ReTPiWY.png");
+  loadSprite("pipe-top-right", "hj2GK4n.png");
+  loadSprite("pipe-bottom-left", "c1cYSbt.png");
+  loadSprite("pipe-bottom-right", "nqQ79eI.png");
+   
+  loadSprite("blue-block", "fVscIbn.png");
+  loadSprite("blue-brick", "3e5YRQd.png");
+  loadSprite("blue-steel", "gqVoI2b.png");
+  loadSprite("blue-evil-shroom", "SvV4ueD.png");
+  loadSprite("blue-surprise", "RMqCc1G.png");
+   
+  scene("game", ({ level, score }) => {
     layers(["bg", "obj", "ui"], "obj");
-
-const mario = add([
-    sprite("mario"),
-    pos(100, 100),
-    body(),
-]);
-
-keyDown("left", () => {
-    mario.move(-120, 0);
-});
-
-keyDown("right", () => {
-    mario.move(120, 0);
-});
-
-keyPress("space", () => {
-    if(mario.grounded()) {
-        mario.jump(400);
+   
+    const maps = [
+      [
+        "                                      ",
+        "                                      ",
+        "                                      ",
+        "                                      ",
+        "                                      ",
+        "     %   =*=%=                        ",
+        "                                      ",
+        "                            -+        ",
+        "                    ^   ^   ()        ",
+        "==============================   =====",
+      ],
+      [
+        "£                                       £",
+        "£                                       £",
+        "£                                       £",
+        "£                                       £",
+        "£                                       £",
+        "£        @@@@@@              x x        £",
+        "£                          x x x        £",
+        "£                        x x x x  x   -+£",
+        "£               z   z  x x x x x  x   ()£",
+        "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+      ],
+    ];
+   
+    const levelCfg = {
+      width: 20,
+      height: 20,
+      "=": [sprite("block"), solid()],
+      $: [sprite("coin"), "coin"],
+      "%": [sprite("surprise"), solid(), "coin-surprise"],
+      "*": [sprite("surprise"), solid(), "mushroom-surprise"],
+      "}": [sprite("unboxed"), solid()],
+      "(": [sprite("pipe-bottom-left"), solid(), scale(0.5)],
+      ")": [sprite("pipe-bottom-right"), solid(), scale(0.5)],
+      "-": [sprite("pipe-top-left"), solid(), scale(0.5), "pipe"],
+      "+": [sprite("pipe-top-right"), solid(), scale(0.5), "pipe"],
+      "^": [sprite("evil-shroom"), solid(), "dangerous"],
+      "#": [sprite("mushroom"), solid(), "mushroom", body()],
+      "!": [sprite("blue-block"), solid(), scale(0.5)],
+      "£": [sprite("blue-brick"), solid(), scale(0.5)],
+      z: [sprite("blue-evil-shroom"), solid(), scale(0.5), "dangerous"],
+      "@": [sprite("blue-surprise"), solid(), scale(0.5), "coin-surprise"],
+      x: [sprite("blue-steel"), solid(), scale(0.5)],
+    };
+   
+    const gameLevel = addLevel(maps[level], levelCfg);
+   
+    const scoreLabel = add([
+      text(score),
+      pos(30, 6),
+      layer("ui"),
+      {
+        value: score,
+      },
+    ]);
+   
+    add([text("level " + parseInt(level + 1)), pos(40, 6)]);
+   
+    function big() {
+      let timer = 0;
+      let isBig = false;
+      return {
+        update() {
+          if (isBig) {
+            CURRENT_JUMP_FORCE = BIG_JUMP_FORCE;
+            timer -= dt();
+            if (timer <= 0) {
+              this.smallify();
+            }
+          }
+        },
+        isBig() {
+          return isBig;
+        },
+        smallify() {
+          this.scale = vec2(1);
+          CURRENT_JUMP_FORCE = JUMP_FORCE;
+          timer = 0;
+          isBig = false;
+        },
+        biggify(time) {
+          this.scale = vec2(2);
+          timer = time;
+          isBig = true;
+        },
+      };
     }
-});
-
-const enemy = add([
-    sprite("enemy"),
-    pos(300,100),
-    body(),
-    { dir: -1 }
-]);
-
-enemy.action(() => {
-    enemy.move(enemy.dir *50, 0);
-    if(enemy.pos.x <= 0 || enemy.pos.x >= width()) {
-        enemy.dir *= -1;
-    }
-});
-
-mario.collides("enemy", () => {
-    go("gameover");
-});
-
-const coin = add([
-    sprite("coin"),
-    pos(150, 120),
-    area(),
-    "coin"
-]);
-
-mario.collides("coin", (c) => {
-    destroy(c);
-    score += 1;
-});
-
-const level = [
-    "====================",
-    "=                  =",
-    "=     ==   ==      =",
-    "=       ==         =",
-    "=     ==   ==      =",
-    "=                  =",
-    "===================="
-];
-
-const map = addLevel(level, {
-    width : 32,
-    height: 32,
-    pos: vec2(0,0),
-    "=" : () => [
-        sprite("block"),
-        solid(),
-        { is_block:true}
-    ],
-});
-
-let score = 0;
-
-const scoreLabel = add([
-    text(score),
-    pos(12, 12),
-    layer("ui"),
-    { value: score }
-]);
-
-action("coin", (c) => {
-    scoreLabel.text = score;
-});
-
-mario.action(() => {
-    if(mario.grounded()) {
-        mario.jump(400);
-    }
-});
-
-
-mario.play("run");
-sound("jump", { volume : 0.5 });
-
-const bat = add([
-    sprite("bat"),
-    pos(200,100),
-    body(),
-    { dir: 1 }
-]);
-
-bat.action(() => {
-    bat.move(bat.dir * 40, 0);
-    if(bat.pos.x <= 0 || bat.pos.x >= width()) {
-        bat.dir *= -1;
-    }
-});
-
-const portal = add([
-    sprite("portal"),
-    pos(350, 200),
-    area(),
-    "portal"
-]);
-
-mario.collides("portal", () => {
-    go("next_level");
-});
-
-const mushroom = add([
-    sprite("mushroom"),
-    pos(250,150),
-    area(),
-    "mushroom"
-]);
-
-mario.collides("mushroom", (m) => {
-    destroy(m);
-    mario.bigger();
-});
-
-let timeLeft = 30;
-const timerLabel = add([
-    text(timeLeft),
-    pos(400, 12),
-    layer("ui"),
-]);
-
-loop(1, () => {
-    timeLeft -= 1;
-    timerLabel.text = timeLeft;
-    if(timeLeft <= 0) {
-        go("gameover");
-    }
-});
-
-const background = add([
-    sprite("background"),
-    layer("bg"),
-    pos(0, 0),
-]);
-
-const boss = add([
-    sprite("boss"),
-    pos(400, 50),
-    health(10),
-]);
-
-boss.action(() => {
-
-});
-});
-
-scene("gameover", ({}) => {
-    add([text("Game Over"), pos(120,120)]);
-    keyPress("space", () => {
-        go("game");
+   
+    const player = add([
+      sprite("mario"),
+      solid(),
+      pos(30, 0),
+      body(),
+      big(),
+      origin("bot"),
+    ]);
+   
+    action("mushroom", (m) => {
+      m.move(20, 0);
     });
-});
-
-
-scene("next_level", ({}) => {
-    go("game", { level: 2 })
-});
-
-localStorage.setItem("score", score);
-start("main_menu");
-debug.log("Game started!");
+   
+    player.on("headbump", (obj) => {
+      if (obj.is("coin-surprise")) {
+        gameLevel.spawn("$", obj.gridPos.sub(0, 1));
+        destroy(obj);
+        gameLevel.spawn("}", obj.gridPos.sub(0, 0));
+      }
+      if (obj.is("mushroom-surprise")) {
+        gameLevel.spawn("#", obj.gridPos.sub(0, 1));
+        destroy(obj);
+        gameLevel.spawn("}", obj.gridPos.sub(0, 0));
+      }
+    });
+   
+    player.collides("mushroom", (m) => {
+      destroy(m);
+      player.biggify(6);
+    });
+   
+    player.collides("coin", (c) => {
+      destroy(c);
+      scoreLabel.value++;
+      scoreLabel.text = scoreLabel.value;
+    });
+   
+    action("dangerous", (d) => {
+      d.move(-ENEMY_SPEED, 0);
+    });
+   
+    player.collides("dangerous", (d) => {
+      if (isJumping) {
+        destroy(d);
+      } else {
+        go("lose", { score: scoreLabel.value });
+      }
+    });
+   
+    player.action(() => {
+      camPos(player.pos);
+      if (player.pos.y >= FALL_DEATH) {
+        go("lose", { score: scoreLabel.value });
+      }
+    });
+   
+    player.collides("pipe", () => {
+      keyPress("down", () => {
+        go("game", {
+          level: (level + 1) % maps.length,
+          score: scoreLabel.value,
+        });
+      });
+    });
+   
+    keyDown("left", () => {
+      player.move(-MOVE_SPEED, 0);
+    });
+   
+    keyDown("right", () => {
+      player.move(MOVE_SPEED, 0);
+    });
+   
+    player.action(() => {
+      if (player.grounded()) {
+        isJumping = false;
+      }
+    });
+   
+    keyPress("space", () => {
+      if (player.grounded()) {
+        isJumping = true;
+        player.jump(CURRENT_JUMP_FORCE);
+      }
+    });
+  });
+   
+  scene("lose", ({ score }) => {
+    add([text(score, 32), origin("center"), pos(width() / 2, height() / 2)]);
+  });
+   
+  start("game", { level: 0, score: 0 });
